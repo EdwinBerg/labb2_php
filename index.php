803304cdc09cea3bb601cbb8d6311481424cb38c
Edwin Berg <edwin_elfsborg@hotmail.com>
2020-05-28 13:58:41 +0200

    updated

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>labb2</title>
+</head>
+<body>
+    <center>
+        <h2>IK Svalan</h2>
+    </center>
+    
+</body>
+</html>
 <?php
 session_start();
 //session_destroy();
@@ -16,16 +30,17 @@ if(isset($_POST['login'])) {
     $sth->execute([':email' => $_POST['email'], ':password' => $_POST['password']]);
     $result = $sth->fetchAll();
 
+    // kollar om användaren finns
     if($result[0]['user_id'] > 0) {
-        echo "<p>Inloggad!</p>";
         $_SESSION['checkLogin'] = 1;
     } else {
         echo "<p>Fel lösenord eller email</p>";
     }
-}   
     
+
+}   
+    // kollar om användaren är inloggad
 if($_SESSION['checkLogin'] == 1) {
-    // visa alla medlemmar och gör en logga ut knapp
     ?>
         <form method="post"> 
             <input type="submit" value="Logout" name="logout">
@@ -35,6 +50,37 @@ if($_SESSION['checkLogin'] == 1) {
         session_destroy();
         echo "<p>Utloggad!</p>";
     }
+
+// lägger till 3st sporter
+?>
+    <form method="post">
+        <input type="submit" value="Fotboll" name="fotboll">
+    </form>
+
+    <form method="post">
+        <input type="submit" value="Skidor" name="skidor">
+    </form>
+
+    <form method="post">
+        <input type="submit" value="Gymnastik" name="gymnastik">
+    </form>
+
+    <form method="post">
+        <input type="submit" value="Medlemmar" name="medlemmar">
+    </form>
+<?php 
+
+// Om fotboll är valt ||||| Lägg till så att man kollar om användaren är admin och äre så så ska en redigera knapp komma upp, antingen vid alla medlemmar eller vid varje sport så-
+// man kan lägga till, tabort eller redigera medlemmar. 
+if(isset($_POST['fotboll'])) {
+    $sql = "SELECT name FROM login WHERE sport = fotboll";
+    $sth = $dbh->prepare($sql);
+    $sth->execute(['sport' => 'fotboll']);
+    $result = $sth->fetchAll();
+    var_dump($result[0]);
+}
+
+
 
 
 
@@ -50,14 +96,3 @@ if($_SESSION['checkLogin'] == 1) {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>labb2</title>
-</head>
-<body>
-    
-</body>
-</html>
