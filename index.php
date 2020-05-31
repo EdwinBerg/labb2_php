c83f0dd5c1143e61fac12239b514d26c361dac12
Edwin Berg <edwin_elfsborg@hotmail.com>
2020-05-31 15:24:40 +0200

    you can now add a member into the db

--- index.php
+++ index.php
@@ -104,9 +104,9 @@ if($_SESSION['checkLogin'] == 1) {
  //lägger till en medlem
  if(isset($_POST['addMember'])) {
      $newMember = $_POST['newMember'];
-     $sql = "INSERT INTO medlem (name) VALUES ($newMember)";
+     $sql = "INSERT INTO medlem (name) VALUES (:name)";
      $sth = $dbh->prepare($sql);
-     $sth->execute([$newMember => 'name']);
+     $sth->execute([':name' => $newMember]);
      $result = $sth->fetchAll();
      echo "<center><p> " . $newMember . " har lagts till!</p></center>";
  }
